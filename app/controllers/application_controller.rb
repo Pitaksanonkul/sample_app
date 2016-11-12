47603cd04a9259acfab3a0e7ac491ee0734b932f
Pitaksanonkul <nprsq.n@gmail.com>
2016-11-12 10:54:07 +0700

    Correct controller file

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,9 +4,7 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   
   def hello
- render html: "hello, world!"
- end
-end 
-
+    render html: "hello, world!"
+  end
 end
 
